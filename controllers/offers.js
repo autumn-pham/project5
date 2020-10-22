4e20fb5bcdf39538cae17a4d210bc48fd8ba63f0
Autumn Pham <autumnpham@Autumns-MacBook.lan>
2020-10-22 08:54:18 -0700

    CRUD working on just offers

--- controllers/offers.js
+++ controllers/offers.js
@@ -4,7 +4,7 @@ const postgres = require('../postgres.js');
 
 
 // EDIT
-offers.get('/offers/:id/edit', (req, res) => {
+offers.get('/:id/edit', (req, res) => {
   postgres.query(`SELECT * FROM offers WHERE id = ${req.params.id};`, (err, results) => {
       res.render('main_activities/edit.html.ejs', {offer: results})
       console.log(err)
@@ -12,17 +12,27 @@ offers.get('/offers/:id/edit', (req, res) => {
 });
 
 // SHOW
-offers.get('/offers/:id', (req, res) => {
+offers.get('/:id', (req, res) => {
   postgres.query(`SELECT * FROM offers WHERE id = ${req.params.id};`, (err, results) => {
-      console.log(results)
+      // console.log(results)
       res.render('main_activities/show.html.ejs', {offer: results})
   });
 });
 
-// UPDATE
-offers.put('/offers/:id', (req, res) => {
-    postgres.query(`UPDATE offers SET firstname = '${req.body.firstname}', lastname = '${req.body.lastname}', AGE = ${req.body.age}, CITY = ${req.body.city}, SPORT = ${req.body.sport}, ACTIVITY = ${req.body.activity}, WHERE id = ${req.params.id}`, (err, results) => {
+// DELETE
+offers.delete('/:id', (req, res) => {
+    postgres.query(`DELETE FROM offers WHERE id = ${req.params.id};`, (err, results) => {
         postgres.query('SELECT * FROM offers ORDER BY id ASC;', (err, results) => {
+            res.redirect('/activities');
+        });
+    });
+});
+
+// UPDATE
+offers.put('/:id', (req, res) => {
+    postgres.query(`UPDATE offers SET firstname = '${req.body.firstname}', lastname = '${req.body.lastname}', AGE = '${req.body.age}', CITY = '${req.body.city}', SPORT = '${req.body.sport}', ACTIVITY = '${req.body.activity}', WHERE id = '${req.params.id}'`, (err, results) => {
+        postgres.query('SELECT * FROM offers ORDER BY id ASC;', (err, results) => {
+          console.log(err)
           res.redirect('/activities');
         });
     })
@@ -39,7 +49,6 @@ offers.get('/', (req, res) => {
 offers.post('/', (req, res) => {
   postgres.query(`INSERT INTO offers (firstname, lastname, age, city, sport, activity) VALUES ('${req.body.firstname}', '${req.body.lastname}', '${req.body.age}', '${req.body.city}', '${req.body.sport}', '${req.body.activity}')`, (err, results) => {
   postgres.query('SELECT * FROM offers ORDER BY id ASC;', (err, results) => {
-    console.log(err);
     res.redirect('/activities');
   });
   })
