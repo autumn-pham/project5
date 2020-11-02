d43d2e87f41cba4724997cb40a59c35ffd7e9c34
Autumn Pham <autumnpham@Autumns-MacBook.lan>
2020-11-01 17:50:21 -0800

    second AJAX call working

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 $(() => {
+  // AJAX CALL ONE
   $('.searches-search').on('submit', (event)=>{
     event.preventDefault();
     let userInput = $('input[type="text"][name="get-searches"]').val();
@@ -39,12 +40,62 @@ $(() => {
           console.log(error);
         }
        )
+
        $('.reset-btn').click(function(){
       $('main').find('div').remove();
         })
-    
+
 
    })
 
+// AJAX CALL 2
+
+$('.offers-search').on('submit', (event)=>{
+  event.preventDefault();
+  let userInput = $('input[type="text"][name="get-offers"]').val();
+  $(event.currentTarget).trigger('reset')
+  console.log(userInput);
+
+  $.ajax({
+    url: '/data/offers'
+
+  }).then(
+    (response)=>{
+      console.log(response.rows)
+      let offerData = [];
+
+
+      for(let i=0; i < response.rows.length; i++){
+        console.log(response.rows[i].city)
+        if (response.rows[i].city === userInput) {
+          offerData.push(response.rows[i]);
+        }
+
+      }
+      console.log(offerData)
+      for(let i=0; i < offerData.length; i++){
+        let newDiv = $('<div>').addClass('card search-card').attr('id', i)
+        let newName = $('<h5>').text(offerData[i].firstname + " " + offerData[i].lastname).css('text-align', 'center').addClass('card-body card-text')
+        let newText = $('<p>').text("is searching for some").css('text-align', 'center').addClass('card-body card-text')
+        let newSport = $('<p>').text(offerData[i].sport + " " + offerData[i].activity).css('text-align', 'center').addClass('card-body card-text')
+        $('main').append(newDiv)
+        newDiv.append(newName)
+        newDiv.append(newText)
+        newDiv.append(newSport)
+
+      }
+
+    },
+      (error) => {
+        console.log(error);
+      }
+     )
+
+     $('.reset-btn').click(function(){
+    $('main').find('div').remove();
+      })
+
+
+ })
 
 })
